68600d5aa70d9d7b60ff8e4dff87790f84cf2205
YAGAPIN <yagapin@et.esiea.fr>
2022-03-21 14:40:49 +0100

    4eme Commit

--- webserv.py
+++ webserv.py
@@ -1,8 +1,38 @@
-from typing import Optional
-from fastapi import FastAPI
+import requests
+from PyQt5.QtWidgets import QMessageBox
 
-app = FastAPI()
+class Main():
+    def query(self, hostname):
+        url = "http://%s" % (hostname)
+        r = requests.get(url)
+        if r.status_code == requests.codes.NOT_FOUND:
+            QMessageBox.about(self, "Error", "IP not found")
+        if r.status_code == requests.codes.OK:
+            return r.json()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+
+if __name__ == "__main__":
+
+    main = Main()
+    hostname = "127.0.0.1:8000"
+    res = main.query(hostname)
+    if res:
+        print(res)
+
+from shodan import Shodan
+
+@app.get("/ip/{ip}")
+async def get_ip(ip: str, key: Optional[str] = None):
+    if key is None:
+        return {"Error": "Please provide a valid API key"}
+    else:
+        try:
+            api = Shodan(key)
+            res = api.host(ip)
+            return {
+                "IP": res["ip_str"],
+                "Organization": res["org"],
+                "Country": res["country_name"],
+            }
+        except Exception as e:
+            return {"Error": str(e)}
